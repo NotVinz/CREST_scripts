47eb104f31069d5efa599f06febcfe0b7f7aa5b1
NotVinz <90572371+NotVinz@users.noreply.github.com>
2022-12-30 08:59:36 +0100

    Update batch.sh

--- VMD_conformation_analysis/batch.sh
+++ VMD_conformation_analysis/batch.sh
@@ -1,12 +1,8 @@
-echo > report.txt
+echo -ne "" > report.txt
 for i in optimized*.xyz
 do
-vmd $i -dispdev text -e prova.tcl  > /dev/null 2> /dev/null
-
-nrg=$(head -2 $i | tail -1 | awk '{print $1}')
-
-symm=$(cat /tmp/conformerscript.dat)
-
-echo "$i" "$nrg" "$symm"
-
+	vmd $i -dispdev text -e prova.tcl  > /dev/null 2> /dev/null
+	nrg=$(head -2 $i | tail -1 | awk '{print $1}')
+	symm=$(cat /tmp/conformerscript.dat)
+	echo "$i $nrg $symm" 2>&1 | tee -a report.txt
 done
